ef452f7b40f4501f88d6a78354bd456dceb1f198
StefanBehring <chadhz@gmx.de>
2021-09-18 19:21:05 +0200

    Add side load from localStorage

--- src/BalanceSheet/BalanceSheet.js
+++ src/BalanceSheet/BalanceSheet.js
@@ -2,10 +2,14 @@ import BalanceSide from '../BalanceSide/BalanceSide';
 import './BalanceSheet.css';
 
 const BalanceSheet = () => {
+	// Get sides from localStorage
+	const sides = JSON.parse(localStorage.getItem('balance')).sides;
+
 	return (
 		<main className='main'>
-			<BalanceSide />
-			<BalanceSide />
+			{sides.map((side) => (
+				<BalanceSide key={side} side={side} />
+			))}
 		</main>
 	);
 };
