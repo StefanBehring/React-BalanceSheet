f262a7407aea8fc56435ae24d858ae2c73c910e5
StefanBehring <chadhz@gmx.de>
2021-09-18 20:51:20 +0200

    Add styling and layout for ItemFormular

--- src/ItemFormular/ItemFormular.js
+++ src/ItemFormular/ItemFormular.js
@@ -1,7 +1,39 @@
 import './ItemFormular.css';
 
 const ItemFormular = ({ job, side, type, id }) => {
-	return <div>ItemFormular</div>;
+	return (
+		<main className='main'>
+			<section className='item-formular'>
+				<h2 className='item-formular__title'>Add Item</h2>
+				<form className='item-formular__form'>
+					<label className='item-formular__label' for='title'>
+						Title
+					</label>
+					<input className='item-formular__input' type='text' id='title' />
+					<label className='item-formular__label' for='description'>
+						Description
+					</label>
+					<textarea
+						className='item-formular__input'
+						id='description'
+						rows='4'
+						cols='50'
+					></textarea>
+					<label className='item-formular__label' for='value'>
+						Value
+					</label>
+					<input
+						className='item-formular__input'
+						type='number'
+						id='value'
+						step='0.01'
+						min='0.01'
+					/>
+					<button className='item-formular__button'>Add item</button>
+				</form>
+			</section>
+		</main>
+	);
 };
 
 export default ItemFormular;
