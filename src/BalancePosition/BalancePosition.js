098b0d4176ba75b30437e402a739ca554e778615
StefanBehring <chadhz@gmx.de>
2021-09-18 17:35:11 +0200

    Add styling and content

--- src/BalancePosition/BalancePosition.js
+++ src/BalancePosition/BalancePosition.js
@@ -1,10 +1,13 @@
 import BalanceItem from '../BalanceItem/BalanceItem';
 import './BalancePosition.css';
 
-const BalancePosition = () => {
+const BalancePosition = (title, items) => {
 	return (
-		<div>
-			<h3>Title</h3>
+		<div className='balance-position'>
+			<div className='balance-position__header'>
+				<h3 className='balance-position__title'>Title</h3>
+				<button className='balance-position__add-item'>Add</button>
+			</div>
 			<BalanceItem />
 			<BalanceItem />
 			<BalanceItem />
