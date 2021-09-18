3f56e2ff28d3a801e3ce99627379a70802bdf580
StefanBehring <chadhz@gmx.de>
2021-09-18 19:20:33 +0200

    Edit localStorage setup

--- src/App/App.js
+++ src/App/App.js
@@ -4,65 +4,63 @@ import BalanceSheet from '../BalanceSheet/BalanceSheet';
 function App() {
 	// LocalStorage init
 	if (localStorage.getItem('balance') === null) {
-		const newBalance = [
-			{
-				side: ['activa', 'passiva'],
-				types: ['long-term', 'mid-term', 'short-term'],
-				items: [
-					{
-						id: 0,
-						side: 'activa',
-						type: 'long-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-					{
-						id: 1,
-						side: 'activa',
-						type: 'mid-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-					{
-						id: 2,
-						side: 'activa',
-						type: 'short-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-					{
-						id: 3,
-						side: 'passiva',
-						type: 'long-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-					{
-						id: 4,
-						side: 'passiva',
-						type: 'mid-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-					{
-						id: 5,
-						side: 'passiva',
-						type: 'short-term',
-						title: 'appartment',
-						description: 'my appartment',
-						value: '23.123,89',
-					},
-				],
-			},
-		];
+		const newBalance = {
+			sides: ['activa', 'passiva'],
+			types: ['long-term', 'mid-term', 'short-term'],
+			items: [
+				{
+					id: 0,
+					side: 'activa',
+					type: 'long-term',
+					title: 'appartment',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+				{
+					id: 1,
+					side: 'activa',
+					type: 'mid-term',
+					title: 'loan',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+				{
+					id: 2,
+					side: 'activa',
+					type: 'short-term',
+					title: 'bill',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+				{
+					id: 3,
+					side: 'passiva',
+					type: 'long-term',
+					title: 'appartment',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+				{
+					id: 4,
+					side: 'passiva',
+					type: 'mid-term',
+					title: 'loan',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+				{
+					id: 5,
+					side: 'passiva',
+					type: 'short-term',
+					title: 'bill',
+					description: 'my appartment',
+					value: '23.123,89',
+				},
+			],
+		};
 		localStorage.setItem('balance', JSON.stringify(newBalance));
 	}
-	console.table(JSON.parse(localStorage.getItem('balance')));
+
 	return (
 		<div className='app'>
 			<header className='app__header'>
