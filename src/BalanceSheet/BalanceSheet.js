92c737142d29d0652d15a408daa99f00146caf4b
StefanBehring <chadhz@gmx.de>
2021-09-21 17:53:30 +0200

    Add styled components to BalanceSheet

--- src/BalanceSheet/BalanceSheet.js
+++ src/BalanceSheet/BalanceSheet.js
@@ -1,9 +1,25 @@
+import styled from 'styled-components'
 import BalanceSide from '../BalanceSide/BalanceSide'
-import './BalanceSheet.css'
 
 const BalanceSheet = ({ balanceLocalStorage, onChangePage, onDeleteItem }) => {
   // Get sides from localStorage
   const sides = balanceLocalStorage.sides
+
+  const Main = styled.main`
+    background-color: var(--color-light);
+    border: 1px solid var(--color-button-border);
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+    margin: 1rem auto;
+    width: 320px;
+
+    @media screen and (min-width: 760px) {
+      flex-direction: row;
+      width: 720px;
+    }
+  `
 
   return (
     <main className="main">
